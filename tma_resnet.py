9c5f8908e61e6494d498377d95c65a8df9ad3bed
Gideon Dresdner <gideond@gmail.com>
2016-06-08 17:40:52 -0400

    conv net working on mnist
    - learned a lot about computation graphs and tf's model.
    - next is to implement residual units and skip-connections.

--- tma_resnet.py
+++ tma_resnet.py
@@ -6,12 +6,13 @@ from collections import namedtuple
 from math import sqrt
 import os
 
-# from sklearn import metrics
 import tensorflow as tf
 from tensorflow.contrib import learn
 from tensorflow.examples.tutorials.mnist import input_data
 
-CONFIG = {'use_cudnn_on_gpu': True,
+import numpy as np
+
+CONFIG = {'use_cudnn_on_gpu': True, # TODO
           'chip': '/gpu:0',
           'batch_size': 128,
           'num_classes': 10
@@ -27,7 +28,7 @@ def variable_on_chip(name, shape, initializer, chip=CONFIG['chip']):
         var = tf.get_variable(name, shape, initializer=initializer)
         return var
 
-def resnet(x, y, activation=tf.nn.relu): # TODO, according to paper don't use relu...
+def inference(x, activation=tf.nn.relu): # TODO, according to paper don't use relu...
     with tf.variable_scope('layer1') as scope:
         num_filters1 = 64
         num_examples, height, width, num_channels = x.get_shape().as_list()
@@ -57,9 +58,8 @@ def resnet(x, y, activation=tf.nn.relu): # TODO, according to paper don't use re
 
     with tf.variable_scope('layer3') as scope:
         layer3_dim = 128
-        reshape = tf.reshape(out2, [-1])
-        print("out2", out2)
-        print("reshape", reshape)
+        squashed_dim = reduce(lambda x, y: x*y, out2.get_shape().as_list()[1:])
+        reshape = tf.reshape(out2, [-1, squashed_dim])
         input_dim = reshape.get_shape().as_list()[1]
 
         weights = variable_on_chip('weights', [input_dim, layer3_dim],
@@ -77,17 +77,68 @@ def resnet(x, y, activation=tf.nn.relu): # TODO, according to paper don't use re
         biases = variable_on_chip('biases', [CONFIG['num_classes']],
                                   tf.constant_initializer(0.0))
 
-        pred = tf.nn.relu(tf.matmul(dense3, weights) + biases, name=scope.name)
-    return learn.models.logistic_regression(pred, y)
+        preds = tf.nn.relu(tf.matmul(dense3, weights) + biases, name=scope.name)
 
-ndim = int(sqrt(mnist.train.images.shape[1]))
-x_train = mnist.train.images.reshape([-1, ndim, ndim, 1])
-# net = resnet(x_train, mnist.train.labels)
+    return preds
 
-classifier = learn.TensorFlowEstimator(
-    model_fn=resnet, n_classes=10, batch_size=CONFIG['batch_size'], steps=100,
-    learning_rate=0.001, continue_training=True, optimizer='SGD')
+def loss(preds, labels):
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(preds, labels, name='crossentropy')
+    the_loss = tf.reduce_mean(cross_entropy, name='crossentropy_mean')
+    return the_loss
 
-classifier.fit(x_train, mnist.train.labels)
+def train(loss, learning_rate):
+    tf.scalar_summary(loss.op.name, loss)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    train_op = optimizer.minimize(loss, global_step=global_step)
+    return train_op
+
+def accuracy(preds, labels, k=1):
+    correct = tf.nn.in_top_k(preds, labels, k)
+    correct = tf.cast(correct, tf.int32)
+    return tf.reduce_sum(correct)
+    
+with tf.Session() as sess:
+    ndim = int(sqrt(mnist.train.images.shape[1]))
+    x_placeholder = tf.placeholder(tf.float32, shape=(None, ndim, ndim, 1))
+    y_placeholder = tf.placeholder(tf.int32, shape=(None))
+    
+    preds = inference(x_placeholder)
+    loss_op = loss(preds, y_placeholder)
+    train_op = train(loss_op, 0.01)
+    accuracy_op = accuracy(preds, y_placeholder)
+
+    # TODO saver, summary
+
+    init = tf.initialize_all_variables()
+    sess.run(init)
+
+    x_train = mnist.train.images.reshape([-1, ndim, ndim, 1])
+    y_train = mnist.train.labels
+    num_examples = x_train.shape[0]
+    num_batches = int(x_train.shape[0] / CONFIG['batch_size'])+1
+    for batch_i in xrange(num_batches):
+        start = batch_i*CONFIG['batch_size']
+        end = (batch_i+1)*CONFIG['batch_size']
+        x_batch = x_train[start:end]
+
+        y_batch = mnist.train.labels[start:end]
+
+        sess.run([train_op, loss_op], feed_dict={x_placeholder: x_batch,
+                                              y_placeholder: y_batch})
+
+
+        random_sample = np.random.randint(num_examples, size=(1000,))
+
+        print("training accuracy:\t",
+         sess.run(accuracy_op, feed_dict={x_placeholder: x_train[random_sample],
+                                          y_placeholder: y_train[random_sample]}) / 1000.0)
+
+        
+        print("test accuracy:\t\t",
+         sess.run(accuracy_op, feed_dict={x_placeholder: mnist.test.images.reshape(-1,ndim,ndim,1),
+                                          y_placeholder: mnist.test.labels})  / float(mnist.test.images.shape[0]))
+        print()
+
 
 tf.reset_default_graph() # TODO
