6cb5cf67fefbd5b08d1dc7beca431dc7f9957c33
Gideon Dresdner <gideond@gmail.com>
2016-06-14 17:29:59 -0400

    patch iterator

--- cedar_sinai_etl.py
+++ cedar_sinai_etl.py
@@ -2,8 +2,28 @@
 Handles the ETL for Ciders-Sinai dataset.
 """
 
+import cv2
+import numpy as np
 import os
 path = os.path.expandvars("$TMPDIR/TIFF color normalized sequential filenames/test100.tif")
 
-with open(path) as f:
-    print(f)
+patch_size = 29
+stride = 10
+
+img = cv2.imread(path)
+
+def patch_iter(img, patch_size, stride):
+    '''
+    Takes an image represented by a numpy array of dimensions [l, w,
+    channel], and creates an iterator over all patches in the imagine
+    defined by the `patch_size` and the `stride` length.
+    '''
+    # TODO unit test
+    for x in range((img.shape[0]-patch_size+1)/stride):
+        for y in range((img.shape[1]-patch_size+1)/stride):
+            patch = img[x*stride:x*stride+patch_size, y*stride:y*stride+patch_size, :]
+            yield patch
+
+patches = patch_iter(img, patch_size, stride)
+
+
