c7d43885d8dbc2fc642a1120d65499ffbc3dc8f2
Gideon Dresdner <gideond@gmail.com>
2016-06-14 17:04:06 -0400

    script for extracting data to tmpdir

---
+++ extract_data_on_hal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp /cbio/ski/fuchs/home/dresdnerg/data/CedersSinai_PCa.zip /scratch/7469793.hal-sched1.local
+pushd .
+cd /scratch/7469793.hal-sched1.local
+unzip CedersSinai_PCa.zip
+popd
