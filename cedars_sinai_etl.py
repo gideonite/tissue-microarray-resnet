6fa089c95418759fc4cfbc874b905bd03165e461
Gideon Dresdner <gideond@gmail.com>
2016-06-15 19:04:47 -0400

    screw iterators.
    i have big computers and small data. load it all.

--- cedars_sinai_etl.py
+++ cedars_sinai_etl.py
@@ -2,7 +2,6 @@
 Handles the ETL for Ciders-Sinai dataset.
 """
 
-from collections import namedtuple
 import cv2
 import itertools
 import numpy as np
@@ -10,43 +9,67 @@ import random
 import scipy.io as sio
 import os
 
-random.seed(1337)
+random.seed(1337) # TODO
 
 patch_size = 29
 stride = 10
+img_filename = "/mnt/data/TIFF color normalized sequential filenames/test%d.tif"
+label_filename = "/mnt/data/ATmask sequential filenames/test%d_Mask.mat"
 
-def patch_iter(imgfilename, patch_size, stride):
+def _patches(imgfilename, patch_size, stride):
     '''
     Takes an image represented by a numpy array of dimensions [l, w,
     channel], and creates an iterator over all patches in the imagine
     defined by the `patch_size` and the `stride` length.
     '''
-    # TODO unit test
-    img = cv2.imread(imgfilename)
+    img = cv2.imread(imgfilename).astype('float32')
+    ret = []
     for x in range((img.shape[0]-patch_size+1)/stride):
         for y in range((img.shape[1]-patch_size+1)/stride):
             patch = img[x*stride:x*stride+patch_size, y*stride:y*stride+patch_size]
-            yield patch
+            ret.append(patch.flatten())
 
-def center_of_patch(patch):
-    length = patch.shape[0]
-    width = patch.shape[1]
-    return patch[length/2,width/2]
+    return ret
 
-def patch_center_iter(imgfilename, patch_size, stride):
-    img = cv2.imread(imgfilename)
-    for patch in patch_iter(img, patch_size, stride):
-        yield patch[patch_size/2, patch_size/2]
+def _patch_labels(matfilename, patch_size, stride):
+    '''
+    Takes a patch of pixel-wise labels and extracts the representative
+    label, namely the center of the patch.
+    '''
+    img = sio.loadmat(matfilename)['ATmask']
+    ret = []
+    for x in range((img.shape[0]-patch_size+1)/stride):
+        for y in range((img.shape[1]-patch_size+1)/stride):
+            patch = img[x*stride:x*stride+patch_size, y*stride:y*stride+patch_size]
+            ret.append(patch[patch_size/2, patch_size/2] - 1)
+    return ret
 
-xdata = []
-ydata = []
-img_filename = "$TMPDIR/TIFF color normalized sequential filenames/test%d.tif"
-label_filename = "$TMPDIR/Color annotation sequential filenames/test%d_Annotated.tif"
-file_nums = list(range(1,256))
+file_nums = list(range(1,225))
 random.shuffle(file_nums)
-for file_num in file_nums:
-    xdata.append(patch_iter(os.path.expandvars(img_filename %(file_num)), patch_size, stride))
-    ydata.append(patch_center_iter(os.path.expandvars(label_filename %(file_num)), patch_size, stride))
+def dataset(num_images=len(file_nums), train_test_split=0.8):
+    xtrain = []
+    ytrain = []
+    xtest = []
+    ytest = []
 
-DataSet = namedtuple("DataSet", ["xdata", "ydata"])
-cedars_sinai_data = DataSet(itertools.chain(*xdata), itertools.chain(*ydata))
+    for file_num in file_nums[:int(train_test_split * num_images)]:
+        patches = _patches(img_filename %(file_num), patch_size, stride)
+        labels = _patch_labels(label_filename %(file_num), patch_size, stride)
+
+        assert len(patches) == len(labels)
+
+        for i in xrange(len(patches)):
+            xtrain.append(patches[i])
+            ytrain.append(labels[i])
+
+    for file_num in file_nums[int(train_test_split * num_images):num_images]:
+        patches = _patches(img_filename %(file_num), patch_size, stride)
+        labels = _patch_labels(label_filename %(file_num), patch_size, stride)
+
+        assert len(patches) == len(labels)
+
+        for i in xrange(len(patches)):
+            xtest.append(patches[i])
+            ytest.append(labels[i])
+
+    return np.array(xtrain), np.array(ytrain), np.array(xtest), np.array(ytest)
