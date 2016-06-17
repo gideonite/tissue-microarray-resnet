98ed70c4e4078dd68cdcaee0383be86110a6ef39
Gideon Dresdner <gideond@gmail.com>
2016-06-17 16:33:23 -0400

    log stuff
    need to actually figure out how to save models.

--- main_pure.py
+++ main_pure.py
@@ -3,6 +3,7 @@ from sklearn.cross_validation import train_test_split
 import tensorflow as tf
 import json
 from math import sqrt
+import time
 
 import cedars_sinai_etl
 import resnet_pure
@@ -10,6 +11,9 @@ import resnet_pure
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
+timestamp = time.time()
+
+flags.DEFINE_string('training_log_dir', '/mnt/code/notebooks/results/' + str(timestamp) = '.json')
 flags.DEFINE_string('model_cache_dir', '/mnt/data/models/', 'Directory to save models and summaries')
 flags.DEFINE_integer('num_epochs', 20, 'Number of times to go over the dataset')
 flags.DEFINE_integer('batch_size', 64, 'Number of examples per GD batch')
@@ -19,7 +23,12 @@ def main(_):
     ndim = int(sqrt(xdata.shape[1] / 3))
     xdata = xdata.reshape(-1, ndim, ndim, 3)
 
-    print(xdata.shape)
+    log = {training_acc: [],
+           'num_epochs': 20,
+           'timestamp': timestamp,
+           'batch_size': FLAGS.batch_size,
+           'patch_size': FLAGS.patch_size,
+           'stride': FLAGS.stride}
 
     with tf.Session() as sess:
         for accs in resnet_pure.train(xdata, ydata,
@@ -27,9 +36,14 @@ def main(_):
                                       FLAGS.batch_size,
                                       FLAGS.num_epochs,
                                       sess,
+                                      model_cache_dir=model_cache_dir,
                                       optimizer=tf.train.GradientDescentOptimizer,
                                       learning_rate=0.01):
-            print(accs)
+            log['training_acc'].append(accs)
+
+            # overwrite the log file each time.
+            with open(FLAGS.training_log_dir, 'w+') as logfile:
+                json.dump(log, logfile)
         
 if __name__ == '__main__':
     tf.app.run()
