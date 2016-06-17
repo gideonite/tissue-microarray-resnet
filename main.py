6d11fad090451c77cf08cfd045906774e8a5ee13
Gideon Dresdner <gideond@gmail.com>
2016-06-16 21:51:51 -0400

    iterate over batches manually
    but takes up an insane amount of memory. wtf.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn import metrics
 from sklearn.cross_validation import train_test_split
 import tensorflow as tf
 from tensorflow.contrib import learn
+import json
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -15,37 +16,54 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('model_cache_dir', '/mnt/data/models/cedars-sinai-resnet', 'Directory to save models and summaries')
 flags.DEFINE_integer('num_epochs', 20, 'Number of times to go over the dataset')
 flags.DEFINE_string('optimizer', 'SGD', 'Which optimizer to use for GD. Choose from SGD, Adagrad, ADAM')
+flags.DEFINE_integer('batch_size', 64, 'Number of examples per GD batch')
 
 def main(_):
     classifier = learn.TensorFlowEstimator(
-        model_fn=res_net, n_classes=4, batch_size=100, steps=100,
+        model_fn=res_net, n_classes=4, batch_size=FLAGS.batch_size, steps=100,
         learning_rate=0.001, continue_training=True)
 
     xdata, ydata = dataset(num_images=50, train_test_split=1)[:2] # TODO
 
     xtrain, xtest, ytrain, ytest = train_test_split(
         xdata, ydata, test_size=0.2, random_state=42)
+
+    num_examples = xtrain.shape[0]
+    
+    accuracies = []
+    for epoch in xrange(FLAGS.num_epochs):
+        accuracies.append([])
+        for batch_i in xrange(num_examples / FLAGS.batch_size):
+            xbatch = xtrain[batch_i * FLAGS.batch_size : (batch_i+1) * FLAGS.batch_size,:]
+            ybatch = ytrain[batch_i * FLAGS.batch_size : (batch_i+1) * FLAGS.batch_size]
+            classifier.fit(xbatch, ybatch, logdir=FLAGS.model_cache_dir)
+
+            tr_accuracy = metrics.accuracy_score(
+                ytrain, classifier.predict(xtrain, batch_size=100))
+            te_accuracy = metrics.accuracy_score(
+                ytest, classifier.predict(xtest, batch_size=100))
+
+            accuracies[-1].append({'train_accuracy': tr_accuracy, 'test_accuracy': te_accuracy})
+
+        # last batch
+        xbatch = xtrain[(batch_i+1) * FLAGS.batch_size:, :]
+        ybatch = ytrain[(batch_i+1) * FLAGS.batch_size:]
+        classifier.fit(xbatch, ybatch, logdir=FLAGS.model_cache_dir)
+        tr_accuracy = metrics.accuracy_score(
+            ytrain, classifier.predict(xtrain, batch_size=100))
+        te_accuracy = metrics.accuracy_score(
+            ytest, classifier.predict(xtest, batch_size=100))
+        accuracies[-1].append({'train_accuracy': tr_accuracy, 'test_accuracy': te_accuracy})
+
+        with open(FLAGS.model_cache_dir + '-training.json') as accuracy_file:
+            json.dump(accuracies, accuracy_file)
+
+        classifier.save(FLAGS.model_cache_dir)
+
+        print('Epoch: ' + str(epoch))
+        print('Accuracy: {0:f}'.format(score))
+        print('Training Error: {0:f}'.format(tr_error))
         
-    # for epoch in xrange(FLAGS.num_epochs):
-    #     classifier.fit(
-    #         xtrain, ytrain, logdir=FLAGS.model_cache_dir)
-
-    #     # Calculate accuracy and training error
-    #     score = metrics.accuracy_score(
-    #         ytest, classifier.predict(xtest, batch_size=100))
-    #     tr_error = metrics.accuracy_score(
-    #         ytrain, classifier.predict(xtrain, batch_size=100))
-    #     print('Accuracy: {0:f}'.format(score))
-    #     print('Training Error: {0:f}'.format(tr_error))
-    #     with open(FLAGS.model_cache_dir + 'training.log', 'a') as accuracy_file:
-    #         accuracy_file.write('Accuracy: {0:f}'.format(score))
-    #         accuracy_file.write('\n')
-
-    #         accuracy_file.write('Training Error: {0:f}'.format(tr_error))
-    #         accuracy_file.write('\n')
-
-        # classifier.save(FLAGS.model_cache_dir)
-
 def reset_flags():
     '''
     Courtesy of https://gist.github.com/wookayin/6ca93b8309f05055c6a3
