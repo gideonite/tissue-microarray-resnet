3b26b5c87d17b54dca76b80ddb2a68573e25478d
Gideon Dresdner <gideond@gmail.com>
2016-06-17 13:26:51 -0400

    appears to be training. still not sure what to do about batch_sizes

--- resnet2.py
+++ resnet2.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from math import sqrt
@@ -11,8 +12,18 @@ def conv2d(x, filter_shape, num_channels, stride):
     weights = get_variable('weights', filter_shape + [x.get_shape()[-1].value, num_channels])
     conv = tf.nn.conv2d(x, weights, [1,1,1,1], padding='SAME')
     mean, variance = tf.nn.moments(conv, axes=[0,1,2])
-    batch_norm = tf.nn.batch_normalization(conv, mean, variance, offset=None, scale=None, variance_epsilon=0.0001)
+    batch_norm = tf.nn.batch_normalization(conv, mean, variance,
+                                           offset=None, scale=None, variance_epsilon=0.0001)
     return tf.nn.relu(batch_norm)
+
+# Configurations for each bottleneck group.
+BottleneckGroup = namedtuple(
+    'BottleneckGroup', ['num_blocks', 'num_filters', 'bottleneck_size'])
+groups = [BottleneckGroup(3, 128, 32),
+        BottleneckGroup(3, 256, 64),
+        BottleneckGroup(3, 512, 128),
+        BottleneckGroup(3, 1024, 256)
+]
 
 def inference(xplaceholder):
     with tf.variable_scope('layer1') as scope:
@@ -24,10 +35,46 @@ def inference(xplaceholder):
     with tf.variable_scope('layer3') as scope:
         conv3 = conv2d(conv2, [3, 3], 64, [1,1,1,1])
 
-    return conv3
+    with tf.variable_scope('skip_connection') as scope:
+        skip_connection = conv2d(xplaceholder, [1, 1], 64, [1,1,1,1]) + conv3
+
+    with tf.variable_scope('fully_connected') as scope:
+        volumn = 1
+        for dim in skip_connection.get_shape()[1:]:
+            volumn *= dim.value
+        reshape = tf.reshape(skip_connection, [-1, volumn])
+
+        weights = get_variable('weights', [reshape.get_shape()[-1].value, 10])
+        biases = tf.get_variable('biases', [10], initializer=tf.constant_initializer(0.0))
+
+        return tf.nn.relu(tf.matmul(reshape, weights) + biases)
 
 with tf.Session() as sess:
+    batch_size = 128
     ndim = int(sqrt(mnist.train.images.shape[1]))
-    xplaceholder = tf.placeholder(tf.float32, shape=(None, ndim, ndim, 1))
+    xplaceholder = tf.placeholder(tf.float32, shape=(batch_size, ndim, ndim, 1))
+    yplaceholder = tf.placeholder(tf.int32, shape=(batch_size))
 
-    print(inference(xplaceholder))
+    preds = inference(xplaceholder)
+
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(preds, yplaceholder, name='crossentropy')
+    avg_loss = tf.reduce_mean(cross_entropy, name='batchwise_avg_loss')
+
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    train_op = optimizer.minimize(avg_loss, global_step=global_step)
+
+    init = tf.initialize_all_variables()
+    sess.run(init)
+
+    xtrain = mnist.train.images.reshape([-1, 28, 28, 1])
+    ytrain = mnist.train.labels
+    num_examples = xtrain.shape[0]
+
+    for batch_i in xrange(0, num_examples, batch_size):
+        xbatch = xtrain[batch_i : batch_i + batch_size]
+        ybatch = ytrain[batch_i : batch_i + batch_size]
+
+        t, a = sess.run([train_op, avg_loss], feed_dict={xplaceholder: xbatch, yplaceholder: ybatch})
+
+        # print("training loss: ", a)
