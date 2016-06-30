fa4b99128f868b05c73db0e88249d7f55c3c4598
Gideon Dresdner <gideond@gmail.com>
2016-06-30 14:04:04 -0400

    wow, everything sucks
    while implementing model restore discovered a pernicious bug: test_acc
    was actually the last train_acc => all results bogus. Need to start
    over. fml.

--- main_pure.py
+++ main_pure.py
@@ -21,6 +21,26 @@ flags.DEFINE_integer('num_epochs', 20, 'Number of times to go over the dataset')
 flags.DEFINE_integer('batch_size', 64, 'Number of examples per GD batch')
 flags.DEFINE_integer('num_images', 10, '')
 
+def maybe_load_logfile(path):
+    try:
+        with open(path, 'w+') as logfile:
+            log = json.load(logfile)
+    except (IOError, ValueError):
+        print("creating new experiment from scratch in '" + path + "'")
+        log = {'cmd': " ".join(sys.argv), # TODO, want to separate cmd line args from code to automatically restart experiments.
+               'architecture': resnet_pure.groups,
+               'train_accs': [],
+               'test_accs': [],
+               'num_epochs': FLAGS.num_epochs,
+               'num_images': FLAGS.num_images,
+               'timestamp': timestamp,
+               'experiment_name': FLAGS.experiment_name,
+               'batch_size': FLAGS.batch_size,
+               'patch_size': FLAGS.patch_size,
+               'stride': FLAGS.stride}
+
+    return log
+
 def main(_):
     if FLAGS.num_images != -1:
         xdata, ydata = cedars_sinai_etl.dataset(num_images=FLAGS.num_images)
@@ -33,17 +53,8 @@ def main(_):
     xtrain, xtest, ytrain, ytest = train_test_split(
         xdata, ydata, test_size=0.2, random_state=42)
 
-    log = {'architecture': resnet_pure.groups,
-           'train_accs': [],
-           'test_accs': [],
-           'num_epochs': FLAGS.num_epochs,
-           'num_images': FLAGS.num_images,
-           'timestamp': timestamp,
-           'experiment_name': FLAGS.experiment_name,
-           'batch_size': FLAGS.batch_size,
-           'patch_size': FLAGS.patch_size,
-           'stride': FLAGS.stride}
-
+    resultspath = FLAGS.results_basepath  + FLAGS.experiment_name + ".json"
+    log = maybe_load_logfile(resultspath)
     json.dump(log, sys.stdout, indent=2)
 
     example = xtrain[0]
@@ -57,9 +68,21 @@ def main(_):
     init = tf.initialize_all_variables()
     saver = tf.train.Saver()
     savepath = FLAGS.cache_basepath + FLAGS.experiment_name
-    resultspath = FLAGS.results_basepath  + FLAGS.experiment_name + ".json"
     with tf.Session() as sess:
         sess.run(init)
+        try:
+            saver.restore(sess, savepath)
+        except ValueError:
+            # test on the randomly intialized model
+            test_accs = []
+            for batch_i in xrange(0, xtest.shape[0], FLAGS.batch_size):
+                xbatch = xtest[batch_i : batch_i + FLAGS.batch_size]
+                ybatch = ytest[batch_i : batch_i + FLAGS.batch_size]
+                test_accs.append(sess.run(accuracy, feed_dict={xplaceholder: xbatch, yplaceholder: ybatch}))
+            test_acc = sum(test_accs) / len(test_accs)
+            log['test_accs'].append(str(test_acc))
+            print("\n%s\t epoch: 0 test_accuracy=%f" %(FLAGS.experiment_name, test_acc))
+
         for epoch_i in xrange(FLAGS.num_epochs):
             train_accs = []
             for batch_i in xrange(0, num_examples, FLAGS.batch_size):
@@ -72,7 +95,7 @@ def main(_):
                 # stringify for JSON serialization
                 train_accs.append(str(train_acc))
 
-                print("epoch: %d batch: %d training_accuracy=%f" %(epoch_i, batch_i/FLAGS.batch_size, train_acc))
+                print("epoch: %d batch: %d training_accuracy=%f" %(epoch_i+1, batch_i/FLAGS.batch_size, train_acc))
 
             saver.save(sess, savepath)
 
@@ -84,8 +107,8 @@ def main(_):
 
             log['train_accs'].append(train_accs)
             test_acc = sum(test_accs) / len(test_accs)
-            log['test_accs'].append(str(train_acc))
-            print("%s\t epoch: %d test_accuracy=%f" %(FLAGS.experiment_name, epoch_i, test_acc))
+            log['test_accs'].append(str(test_acc))
+            print("%s\t epoch: %d test_accuracy=%f" %(FLAGS.experiment_name, epoch_i+1, test_acc))
 
             with open(resultspath, 'w+') as logfile:
                 json.dump(log, logfile, indent=2)
