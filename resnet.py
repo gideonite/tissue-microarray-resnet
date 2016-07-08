e3a33389f2e9388bbfe7114a7cbc0c2cb7d8c230
Gideon Dresdner <gideond@gmail.com>
2016-07-08 16:58:55 -0400

    the 'pure' version has won

--- resnet.py
+++ resnet.py
@@ -1,167 +1,94 @@
-#  Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-
-"""This example builds deep residual network for mnist data.
+"""
+TODO some more docs.
 
 Reference Paper: http://arxiv.org/pdf/1512.03385.pdf
-
-Note that this is still a work-in-progress. Feel free to submit a PR
-to make this better.
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from collections import namedtuple
-from math import sqrt
-import os
-
-from sklearn import metrics
 import tensorflow as tf
-from tensorflow.contrib import learn
-from tensorflow.examples.tutorials.mnist import input_data
+from math import sqrt
 
-def res_net(x, y, activation=tf.nn.relu):
-  """Builds a residual network.
+def get_variable(name, shape):
+    return tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
 
-  Note that if the input tensor is 2D, it must be square in order to be
-  converted to a 4D tensor.
+def fully_connected(x, outdim, activation=tf.nn.relu):
+    indim = x.get_shape()[-1].value
+    weights = get_variable('weights', [indim, outdim])
+    biases = tf.get_variable('biases', [outdim], initializer=tf.constant_initializer(0.0))
+    return tf.matmul(x, weights) + biases
 
-  Borrowed structure from:
-  github.com/pkmital/tensorflow_tutorials/blob/master/10_residual_network.py
+def conv2d(x, filter_shape, num_channels, stride):
+    weights = get_variable('weights', filter_shape + [x.get_shape()[-1].value, num_channels])
+    conv = tf.nn.conv2d(x, weights, stride, padding='SAME')
+    mean, variance = tf.nn.moments(conv, axes=[0,1,2])
+    batch_norm = tf.nn.batch_normalization(conv, mean, variance,
+                                           offset=None, scale=None, variance_epsilon=0.0001)
+    return tf.nn.relu(batch_norm)
 
-  Args:
-    x: Input of the network
-    y: Output of the network
-    activation: Activation function to apply after each convolution
+def flatten(x):
+    volumn = 1
+    for dim in x.get_shape()[1:]:
+        volumn *= dim.value
+    return tf.reshape(x, [-1, volumn])
 
-  Returns:
-    Predictions and loss tensors.
-  """
+# Configurations for each bottleneck group.
+BottleneckGroup = namedtuple(
+    'BottleneckGroup', ['num_blocks', 'num_filters', 'bottleneck_size'])
+groups = [BottleneckGroup(3, 128, 32),
+          BottleneckGroup(3, 256, 64),
+          BottleneckGroup(3, 512, 128),
+          BottleneckGroup(3, 1024, 256)
+]
 
-  # Configurations for each bottleneck group.
-  BottleneckGroup = namedtuple(
-      'BottleneckGroup', ['num_blocks', 'num_filters', 'bottleneck_size'])
-  groups = [BottleneckGroup(3, 128, 32),
-            BottleneckGroup(3, 256, 64),
-            BottleneckGroup(3, 512, 128),
-            BottleneckGroup(3, 1024, 256)
-  ]
-
-  # The first dimension of the input is the batch. If `input_shape ==
-  # 2` then the image is 1D. Reshape it to be 2D.
-  input_shape = x.get_shape().as_list()
-  if len(input_shape) == 2:
-    ndim = int(sqrt(input_shape[1]/3))
-    x = tf.reshape(x, [-1, ndim, ndim, 3])
-
+def inference(xplaceholder, num_classes):
   # First convolution expands to 64 channels
-  with tf.variable_scope('conv_layer1'):
-    net = learn.ops.conv2d(x, 64, [7, 7], batch_norm=True,
-                           activation=activation, bias=False)
+    with tf.variable_scope('first_conv_expand_layer'):
+        net = conv2d(xplaceholder, [7, 7], 64, [1, 1, 1, 1])
+    net = tf.nn.max_pool(
+        net, [1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-  # Max pool
-  net = tf.nn.max_pool(
-      net, [1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+    for group_i, group in enumerate(groups):
+        for block_i in range(group.num_blocks):
+            name = 'group_%d/block_%d' % (group_i, block_i)
+            
+            with tf.variable_scope(name + '/conv_in'):
+                conv = conv2d(net, [1, 1], group.bottleneck_size, [1, 1, 1, 1])
+                
+            with tf.variable_scope(name + '/conv_bottleneck'):
+                conv = conv2d(net, [3, 3], group.bottleneck_size, [1, 1, 1, 1])
 
-  # First chain of resnets
-  with tf.variable_scope('conv_layer2'):
-    net = learn.ops.conv2d(net, groups[0].num_filters,
-                           [1, 1], [1, 1, 1, 1],
-                           padding='VALID', bias=True)
+            with tf.variable_scope(name + '/conv_out'):
+                outdim = net.get_shape()[-1].value
+                conv = conv2d(net, [3, 3], outdim, [1, 1, 1, 1])
 
-  # Create the bottleneck groups, each of which contains `num_blocks`
-  # bottleneck groups.
-  for group_i, group in enumerate(groups):
-    for block_i in range(group.num_blocks):
-      name = 'group_%d/block_%d' % (group_i, block_i)
+            net = conv + net
+        try:
+            next_group = groups[group_i+1]
+            with tf.variable_scope('block_%d/conv_upscale' % group_i):
+                net = conv2d(net, [1, 1], next_group.num_filters, [1, 1, 1, 1])
+        except IndexError:
+            pass
 
-      # 1x1 convolution responsible for reducing dimension
-      with tf.variable_scope(name + '/conv_in'):
-        conv = learn.ops.conv2d(net, group.bottleneck_size,
-                                [1, 1], [1, 1, 1, 1],
-                                padding='VALID',
-                                activation=activation,
-                                batch_norm=True,
-                                bias=False)
+    net_shape = net.get_shape().as_list()
+    net = tf.nn.avg_pool(net, ksize=[1, net_shape[1], net_shape[2], 1],
+                   strides=[1, 1, 1, 1], padding='VALID')
+    net = flatten(net)
+    net = fully_connected(net, num_classes)
+    return net
 
-      with tf.variable_scope(name + '/conv_bottleneck'):
-        conv = learn.ops.conv2d(conv, group.bottleneck_size,
-                                [3, 3], [1, 1, 1, 1],
-                                padding='SAME',
-                                activation=activation,
-                                batch_norm=True,
-                                bias=False)
+def train_ops(xplaceholder,
+          yplaceholder,
+          num_classes,
+          optimizer=tf.train.GradientDescentOptimizer,
+          learning_rate=0.01):
 
-      # 1x1 convolution responsible for restoring dimension
-      with tf.variable_scope(name + '/conv_out'):
-        input_dim = net.get_shape()[-1].value
-        conv = learn.ops.conv2d(conv, input_dim,
-                                [1, 1], [1, 1, 1, 1],
-                                padding='VALID',
-                                activation=activation,
-                                batch_norm=True,
-                                bias=False)
+    preds = inference(xplaceholder, num_classes)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(preds, yplaceholder,
+                                                                   name='crossentropy')
+    avg_loss = tf.reduce_mean(cross_entropy, name='batchwise_avg_loss')
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    train_op = optimizer(learning_rate).minimize(avg_loss, global_step=global_step)
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(preds,1),
+                                               yplaceholder), tf.float32))
 
-      # shortcut connections that turn the network into its counterpart
-      # residual function (identity shortcut)
-      net = conv + net
-
-    try:
-      # upscale to the next group size
-      next_group = groups[group_i + 1]
-      with tf.variable_scope('block_%d/conv_upscale' % group_i):
-        net = learn.ops.conv2d(net, next_group.num_filters,
-                               [1, 1], [1, 1, 1, 1],
-                               bias=False,
-                               padding='SAME')
-    except IndexError:
-      pass
-
-  net_shape = net.get_shape().as_list()
-  net = tf.nn.avg_pool(net,
-                       ksize=[1, net_shape[1], net_shape[2], 1],
-                       strides=[1, 1, 1, 1], padding='VALID')
-
-  net_shape = net.get_shape().as_list()
-  net = tf.reshape(net, [-1, net_shape[1] * net_shape[2] * net_shape[3]])
-
-  return learn.models.logistic_regression(net, y)
-
-if __name__ == "__main__":
-    # Download and load MNIST data.
-    mnist = input_data.read_data_sets('MNIST_data')
-    
-    # Restore model if graph is saved into a folder.
-    if os.path.exists('models/resnet/graph.pbtxt'):
-      classifier = learn.TensorFlowEstimator.restore('models/resnet/')
-    else:
-      # Create a new resnet classifier.
-      classifier = learn.TensorFlowEstimator(
-        model_fn=res_net, n_classes=10, batch_size=100, steps=100,
-        learning_rate=0.001, continue_training=True)
-
-      while True:
-        # Train model and save summaries into logdir.
-        classifier.fit(
-          mnist.train.images, mnist.train.labels, logdir='models/resnet/')
-
-        # Calculate accuracy.
-        score = metrics.accuracy_score(
-          mnist.test.labels, classifier.predict(mnist.test.images, batch_size=64))
-        print('Accuracy: {0:f}'.format(score))
-
-    # Save model graph and checkpoints.
-    classifier.save('models/resnet/')
+    return train_op, preds, avg_loss, accuracy
