3a31e2b9f0f9fb45646892b3dc6983ba082eb8ab
Gideon Dresdner <gideond@gmail.com>
2016-07-15 14:46:26 -0400

    pull in vis, req. for model eval

---
+++ vis.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import sys
+
+basedir = "/home/gideon/Data/cedars-sinai/"
+img_filename = basedir + "TIFF color normalized sequential filenames/test%d.tif"
+raw_label_filename = basedir + "ATmask sequential filenames/test%d_Mask.mat"
+png_label_filename = basedir + "ATmask sequential filenames png/test%d_Mask.png"
+with_annotations_filename = basedir + "Color annotation sequential filenames/test%d_Annotated.tif"
+
+def plot_row_of_images(sample_num):
+    '''
+    Plots a row of raw_image, original annotated img, grayscale annotation mask (4 classes)
+    '''
+    raw_img = cv2.imread(img_filename %(sample_num))
+    annotated_img = cv2.imread(with_annotations_filename %(sample_num))
+    labels_img = cv2.imread(png_label_filename %(sample_num))
+    assert img != None
+
+    fig = plt.figure()
+
+    fig.add_subplot(131)
+    plt.imshow(raw_img)
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+
+    fig.add_subplot(132)
+    plt.imshow(annotated_img)
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+
+    fig.add_subplot(133)
+    plt.imshow(labels_img)
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+
+    plt.show()
