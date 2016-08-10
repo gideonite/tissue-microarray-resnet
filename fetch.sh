3c9eae56d3a19272afb704cce6388921a6f2fb4b
Gideon Dresdner <gideond@gmail.com>
2016-08-09 21:22:46 -0400

    learning curves are up and at them

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # rotation.json (ignoring this because it had to manually fixed because it was killed in mid json dump)
-for json in 6layers_couple.json 18_layers_couple.json flip.json no_augmentation.json flip_rot.json 5layers_couple.json; do
+for json in 4layers_couple.json 6layers_couple.json 18_layers_couple.json flip.json no_augmentation.json flip_rot.json 5layers_couple.json; do
     scp dresdnerg@hal.cbio.mskcc.org:/cbio/ski/fuchs/home/dresdnerg/projects/tissue-microarray-resnet/notebooks/results/$json /tmp
 done
